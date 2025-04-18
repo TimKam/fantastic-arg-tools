52cec28974819ca25c5d28e45f2566ed85534e5a
Timotheus Kampik <timotheus.kampik@gmail.com>
2025-04-18 17:30:15 +0200

    add Jest tests (#10)

---
+++ page/countries.js
@@ -0,0 +1,1 @@
+const countries = ['Austria', 'China', 'Cyprus', 'Finland', 'France', 'Germany', 'Iceland', 'Italy', 'Jordan', 'Luxembourg', 'Netherlands', 'Poland', 'Romania', 'Sweden', 'Switzerland', 'UK']
